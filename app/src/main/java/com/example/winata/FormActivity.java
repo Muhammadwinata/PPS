68b75ea35985dd0916fd6f88a7009d49f4a2acee
Muhammadwinata <ilhambobi96@gmail.com>
2023-10-02 16:27:05 +0700

    prak5

--- app/src/main/java/com/example/winata/FormActivity.java
+++ app/src/main/java/com/example/winata/FormActivity.java
@@ -2,13 +2,61 @@ package com.example.winata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class FormActivity extends AppCompatActivity {
+
+    Button simpan, batal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        simpan = findViewById(R.id.simpan);
+        batal = findViewById(R.id.batal);
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormActivity.this, "Data Disimpan", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        batal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(FormActivity.this);
+
+                alertDialogBuilder.setTitle("Peringatan!");
+
+                alertDialogBuilder.setMessage("Apakah anda yakin untuk menghapus data?")
+                        .setIcon(R.drawable.bes)
+                        .setCancelable(false)
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(), "Data dihapus", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("tidak", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+
+
     }
 }
